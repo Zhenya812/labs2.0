ef90dcf209b8680c08d51e281190845ce517d8fa
I_Zhenya_I <emprybytkov2008@gmail.com>
2026-05-28 23:55:16 +0300

    Add LRU and LFU memoization policies to demo

--- memoization-task3/demo.js
+++ memoization-task3/demo.js
@@ -11,5 +11,29 @@ const memoizedMultiply = memoize(multiply);
 
 console.log(memoizedMultiply(2, 3));
 console.log(memoizedMultiply(2, 3));
-console.log(memoizedMultiply(4, 5));
-console.log(memoizedMultiply(4, 5));
+
+
+console.log("\n Політика LRU");
+
+const lruMemoized = memoize(multiply, {
+    maxSize: 2,
+    policy: "LRU"
+});
+
+console.log(lruMemoized(1, 2));
+console.log(lruMemoized(3, 4));
+console.log(lruMemoized(1, 2));
+console.log(lruMemoized(5, 6));
+
+
+console.log("\n Політика LFU");
+
+const lfuMemoized = memoize(multiply, {
+    maxSize: 2,
+    policy: "LFU"
+});
+
+console.log(lfuMemoized(2, 2));
+console.log(lfuMemoized(2, 2));
+console.log(lfuMemoized(3, 3));
+console.log(lfuMemoized(4, 4));
