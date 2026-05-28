e5c2b20652b7639ba1e762b549156b0064439a93
I_Zhenya_I <emprybytkov2008@gmail.com>
2026-05-29 00:09:10 +0300

    Enhance memoization with TTL and custom eviction
    Added time limit and custom eviction policy to memoization.

--- memoization-task3/demo.js
+++ memoization-task3/demo.js
@@ -5,7 +5,7 @@ function multiply(a, b) {
     return a * b;
 }
 
-console.log("Базова мемоізація");
+console.log(" Базова мемоізація");
 
 const memoizedMultiply = memoize(multiply);
 
@@ -37,3 +37,32 @@ console.log(lfuMemoized(2, 2));
 console.log(lfuMemoized(2, 2));
 console.log(lfuMemoized(3, 3));
 console.log(lfuMemoized(4, 4));
+
+
+console.log("\n Обмеження за часом");
+
+const ttlMemoized = memoize(multiply, {
+    ttl: 2000
+});
+
+console.log(ttlMemoized(5, 5));
+console.log(ttlMemoized(5, 5));
+
+setTimeout(() => {
+    console.log(ttlMemoized(5, 5));
+}, 3000);
+
+
+console.log("\n Власна політика видалення");
+
+const customMemoized = memoize(multiply, {
+    maxSize: 2,
+    policy: "CUSTOM",
+    customEvict: (cache) => {
+        return cache.keys().next().value;
+    }
+});
+
+console.log(customMemoized(6, 6));
+console.log(customMemoized(7, 7));
+console.log(customMemoized(8, 8));
