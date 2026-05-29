9836830ee51f7951606fad559ff5b58089218cbf
I_Zhenya_I <emprybytkov2008@gmail.com>
2026-05-29 13:55:09 +0300

    Implement BiDirectionalPriorityQueue with enqueue, peek, and dequeue

--- Implementing_a_Bi-Directional_Priority_Queue_task4/task4.js
+++ Implementing_a_Bi-Directional_Priority_Queue_task4/task4.js
@@ -13,3 +13,50 @@ class BiDirectionalPriorityQueue {
 
         this.orderCounter++;
     }
+
+    peek(type = "highest") {
+        if (this.items.length === 0) {
+            return null;
+        }
+
+        const index = this.findIndex(type);
+        return this.items[index].item;
+    }
+
+    dequeue(type = "highest") {
+        if (this.items.length === 0) {
+            return null;
+        }
+
+        const index = this.findIndex(type);
+        const removedItem = this.items.splice(index, 1)[0];
+
+        return removedItem.item;
+    }
+
+    findIndex(type) {
+        let selectedIndex = 0;
+
+        for (let i = 1; i < this.items.length; i++) {
+            const current = this.items[i];
+            const selected = this.items[selectedIndex];
+
+            if (type === "highest" && current.priority > selected.priority) {
+                selectedIndex = i;
+            }
+
+            if (type === "lowest" && current.priority < selected.priority) {
+                selectedIndex = i;
+            }
+
+            if (type === "oldest" && current.order < selected.order) {
+                selectedIndex = i;
+            }
+
+            if (type === "newest" && current.order > selected.order) {
+                selectedIndex = i;
+            }
+        }
+
+        return selectedIndex;
+    }
