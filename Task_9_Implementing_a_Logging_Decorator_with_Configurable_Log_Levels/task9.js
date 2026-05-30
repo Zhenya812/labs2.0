12e038ce455582f835ed186d9bf3b8e663403982
I_Zhenya_I <emprybytkov2008@gmail.com>
2026-05-30 20:33:50 +0300

    Update task9.js

--- Task_9_Implementing_a_Logging_Decorator_with_Configurable_Log_Levels/task9.js
+++ Task_9_Implementing_a_Logging_Decorator_with_Configurable_Log_Levels/task9.js
@@ -13,3 +13,17 @@ function defaultFormatter(logData) {
 function jsonFormatter(logData) {
     return JSON.stringify(logData);
 }
+
+function writeLog(message, output = "console") {
+    if (output === "console") {
+        console.log(message);
+    }
+
+    if (output === "file") {
+        fs.appendFileSync("logs.txt", message + "\n", "utf8");
+    }
+
+    if (output === "external") {
+        console.log("Відправлено у зовнішній сервіс:", message);
+    }
+}
