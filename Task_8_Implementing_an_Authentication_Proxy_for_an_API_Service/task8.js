40f06ce65c43445096945b5fa60e0f4caa393e53
I_Zhenya_I <emprybytkov2008@gmail.com>
2026-05-30 16:17:27 +0300

    Add files via upload

---
+++ Task_8_Implementing_an_Authentication_Proxy_for_an_API_Service/task8.js
@@ -0,0 +1,33 @@
+class AuthProxy {
+    constructor(authStrategy) {
+        this.authStrategy = authStrategy;
+        this.requestCount = 0;
+        this.rateLimit = 5;
+    }
+
+    setAuthStrategy(authStrategy) {
+        this.authStrategy = authStrategy;
+    }
+
+    async request(url, options = {}) {
+        if (this.requestCount >= this.rateLimit) {
+            throw new Error("Перевищено ліміт запитів");
+        }
+
+        this.requestCount++;
+
+        const modifiedOptions = {
+            ...options,
+            headers: {
+                ...(options.headers || {}),
+                ...this.authStrategy.getAuthHeaders()
+            }
+        };
+
+        console.log("Виконується запит до:", url);
+        console.log("Метод:", modifiedOptions.method || "GET");
+        console.log("Заголовки авторизації:", modifiedOptions.headers);
+
+        return fakeApiRequest(url, modifiedOptions);
+    }
+}
